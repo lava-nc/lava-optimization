81abab7fa3dd4f3ede71f176e24452a87cc20dcc
Ashish <ashish.rao.mangalore@intel.com>
2021-11-03 19:26:05 +0100

    adding core processes and models of QP solver (without tests)

--- lava/lib/optimization/solvers/qp_solver/models.py
+++ lava/lib/optimization/solvers/qp_solver/models.py
@@ -1,3 +1,140 @@
 # Copyright (C) 2021 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 # See: https://spdx.org/licenses/
+
+import numpy as np
+from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
+from lava.magma.core.model.py.ports import PyInPort, PyOutPort
+from lava.magma.core.model.py.type import LavaPyType
+from lava.magma.core.resources import CPU
+from lava.magma.core.decorator import implements, requires
+from lava.magma.core.model.py.model import PyLoihiProcessModel
+from processes import *
+
+
+@implements(proc=constraintDirections, protocol=LoihiProtocol)
+@requires(CPU)
+class PyDenseModel(PyLoihiProcessModel):
+    s_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.int32, precision=24)
+    a_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, precision=24)
+    weights: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=8)
+
+    def run_spk(self):
+        s_in = self.s_in.recv()
+        # matrix multiplication
+        a_out = self.weights@s_in
+        self.a_out.send(a_out)
+        self.a_out.flush()
+
+@implements(proc=constraintNeurons, protocol=LoihiProtocol)
+@requires(CPU)
+class PyDenseModel(PyLoihiProcessModel):
+    s_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.int32, precision=24)
+    a_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, precision=24)
+    threshold: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+
+    def run_spk(self):
+        s_in = self.s_in.recv()
+        # constraint violation check
+        a_out = (s_in-self.threshold)*(s_in < self.threshold)
+        self.a_out.send(a_out)
+        self.a_out.flush()
+
+@implements(proc=quadraticConnectivity, protocol=LoihiProtocol)
+@requires(CPU)
+class PyDenseModel(PyLoihiProcessModel):
+    s_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.int32, precision=24)
+    a_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, precision=24)
+    weights: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=8)
+
+    def run_spk(self):
+        s_in = self.s_in.recv()
+        #change this line
+        a_out = self.weights@s_in
+        self.a_out.send(a_out)
+        self.a_out.flush()
+
+@implements(proc=solutionNeurons, protocol=LoihiProtocol)
+@requires(CPU)
+class PyDenseModel(PyLoihiProcessModel):
+    s_in_qc: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.int32, precision=24)
+    a_out_qc: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, 
+                                     precision=24)
+    s_in_cn: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.int32, precision=24)
+    a_out_cn: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, 
+                                     precision=24)
+    s_in_alpha: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.int32, 
+                                      precision=24)
+    s_in_beta: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.int32, precision=24)
+    a_out_cc: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, 
+                                     precision=24)
+    qp_neuron_state: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+    grad_bias: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+
+    def run_spk(self):
+        s_in_qc = self.s_in_qc.recv()
+        s_in_cn = self.s_in_cn.recv()
+        s_in_alpha = self.s_in_alpha.recv()
+        s_in_beta = self.s_in_beta.recv()
+        self.qp_neuron_state = -s_in_alpha*(s_in_qc+self.grad_bias) \
+                                -s_in_beta*s_in_cn
+        a_out_cn = self.qp_neuron_state
+        a_out_qc = self.qp_neuron_state
+        a_out_cc = self.qp_neuron_state
+
+        self.a_out_qc.send(a_out_qc)
+        self.a_out_cn.send(a_out_cn)
+        self.a_out_cc.send(a_out_cc)
+        self.a_out_qc.flush()
+        self.a_out_cn.flush()
+        self.a_out_cc.flush()
+
+@implements(proc=constraintNormals, protocol=LoihiProtocol)
+@requires(CPU)
+class PyDenseModel(PyLoihiProcessModel):
+    s_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.int32, precision=24)
+    a_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, precision=24)
+    weights: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=8)
+
+    def run_spk(self):
+        s_in = self.s_in.recv()
+        # matrix multiplication
+        a_out = self.weights@s_in
+        self.a_out.send(a_out)
+        self.a_out.flush()
+
+@implements(proc=learningConstantAlpha, protocol=LoihiProtocol)
+@requires(CPU)
+class PyDenseModel(PyLoihiProcessModel):
+    a_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, precision=24)
+    alpha: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+    decay_counter: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+    alpha_decay_schedule: np.ndarray = LavaPyType(np.ndarray, np.int32, 
+                                                  precision=24)
+    def run_spk(self):
+        self.decay_counter +=1
+        if (self.decay_counter == self.alpha_decay_schedule):
+            self.alpha = np.right_shift(self.alpha, 1)
+            
+            self.decay_counter = np.zeros(self.decay_counter.shape)
+        a_out = self.alpha
+        self.a_out.send(a_out)
+        self.a_out.flush()
+
+@implements(proc=learningConstantBeta, protocol=LoihiProtocol)
+@requires(CPU)
+class PyDenseModel(PyLoihiProcessModel):
+    a_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, precision=24)
+    beta: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+    growth_counter: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+    beta_growth_schedule: np.ndarray = LavaPyType(np.ndarray, np.int32, 
+                                                  precision=24)
+    def run_spk(self):
+        self.growth_counter +=1
+        if (self.growth_counter == self.beta_growth_schedule):
+            self.beta = np.left_shift(self.beta, 1)
+            # TODO: guard against shift overflows
+            self.growth_counter = np.zeros(self.growth_counter.shape)
+        a_out = self.beta
+        self.a_out.send(a_out)
+        self.a_out.flush()
--- lava/lib/optimization/solvers/qp_solver/processes.py
+++ lava/lib/optimization/solvers/qp_solver/processes.py
@@ -1,3 +1,211 @@
 # Copyright (C) 2021 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 # See: https://spdx.org/licenses/
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+# See: https://spdx.org/licenses/
+
+from lava.magma.core.process.process import AbstractProcess
+from lava.magma.core.process.variable import Var
+from lava.magma.core.process.ports.ports import InPort, OutPort
+import numpy as np
+
+class constraintDirections(AbstractProcess):
+    """Connections in the constraint-checking group of neurons. 
+    Realizes the following abstract behavior:
+    a_out = A * s_in
+    """
+
+    def __init__(self, **kwargs):
+        """intialize the constraintDirectionsProcess
+        
+        Kwargs:
+            shape (int tuple): Define the shape of the connections 
+            matrix as a tuple. Defaults to (1,1)
+            constraint_directions (1-D  or 2-D np.array): Define the directions 
+            of the linear constraint hyperplanes. This is 'A' in the constraints 
+            of the QP. Defaults to 0   
+        """
+        super().__init__(**kwargs)
+        shape = kwargs.get("shape", (1, 1))
+        self.s_in = InPort(shape=(shape[1],))      
+        self.a_out = OutPort(shape=(shape[0],))
+        self.weights = Var(shape=shape, 
+                           init=kwargs.pop("constraint_directions", 0)
+                           )
+
+class constraintNeurons(AbstractProcess):
+    """Process to check the violation of the linear constraints of the QP. A 
+    graded spike corresponding to the violated constraint is sent from the out 
+    port.
+    """
+
+    def __init__(self, **kwargs):
+        """Intialize the constraintNeurons Process.
+
+        Kwargs:
+            shape (int tuple): Define the shape of the connections 
+            matrix as a tuple. Defaults to (1,1).
+            thresholds (1-D np.array): Define the thresholds of the neurons 
+            in the constraint checking layer. This is usually 'b' in the 
+            constraints of the QP. Default value of thresholds is 0.
+        """
+        super().__init__(**kwargs)
+        shape = kwargs.get("shape", (1, 1))
+        self.s_in = InPort(shape=(shape[0],))      
+        self.a_out = OutPort(shape=(shape[0],))
+        self.threshold = Var(shape=shape, 
+                             init=kwargs.pop("thresholds", 0)
+                            )
+
+
+
+class quadraticConnectivity(AbstractProcess):
+    """The connections that define the Hessian of the quadratic cost function 
+    Realizes the following abstract behavior:
+    a_out = A * s_in
+    """
+
+    def __init__(self, **kwargs):
+        """Intialize the quadraticConnectivity process.
+        
+        Kwargs:
+            shape (int tuple): A tuple defining the shape of the connections 
+            matrix. Defaults to (1,1).
+            hessian (1-D  or 2-D np.array): Define the hessian matrix ('P' in 
+            the cost function of the QP) in the QP. Defaults to 0.   
+        """
+        super().__init__(**kwargs)
+        shape = kwargs.get("shape", (1, 1))
+        self.s_in = InPort(shape=(shape[1],))      
+        self.a_out = OutPort(shape=(shape[0],))
+        self.weights = Var(shape=shape, 
+                           init=kwargs.pop("hessian", 0))
+
+class solutionNeurons(AbstractProcess):
+    """The neurons that evolve according to the constraint-corrected gradient
+    dynamics. 
+    """
+
+    def __init__(self, **kwargs):
+        """Intialize the solutionNeurons process.
+        
+        Kwargs:
+            shape (int tuple): A tuple defining the shape of the qp neurons. 
+            Defaults to (1,1).
+            qp_neurons_init (1-D np.array): initial value of qp solution neurons
+            grad_bias (1-D np.array): The bias of the gradient of the QP. This 
+            is the value 'p' in the QP definition.          
+        """
+        super().__init__(**kwargs)
+        shape = kwargs.get("shape", (1, 1))
+        # In/outPorts that come from/go to the quadratic connectivity process 
+        self.s_in_qc = InPort(shape=(shape[0],))      
+        self.a_out_qc = OutPort(shape=(shape[0],))
+        # In/outPorts that come from/go to the constraint normals process
+        self.s_in_cn = InPort(shape=(shape[0],))      
+        self.a_out_cn = OutPort(shape=(shape[0],))
+        # Inports for learning constants
+        self.s_in_alpha = InPort(shape=(shape[1],))
+        self.s_in_beta = InPort(shape=(shape[1],))  
+        # OutPort for constraint checking  
+        self.a_out_cc = OutPort(shape=(shape[0],))
+        self.qp_neuron_state = Var(shape=shape,
+                                   init=kwargs.pop("qp_neurons_init", 
+                                                    np.zeros((shape))
+                                                  )
+                                   )
+        self.grad_bias = Var(shape=shape, 
+                             init=kwargs.pop("grad_bias", 
+                                              np.zeros((shape))
+                                            )
+                            )
+
+class constraintNormals(AbstractProcess):
+    """Connections influencing the gradient dynamics when constraints are 
+    violated. 
+    Realizes the following abstract behavior:
+    a_out = A * s_in
+    """
+
+    def __init__(self, **kwargs):
+        """
+        intialize the constraint normals to assign weights to constraint 
+        violation spikes.
+
+        Kwargs:
+            shape (int tuple): A tuple defining the shape of the connections 
+            matrix. Defaults to (1,1).
+            constraint_directions (1-D  or 2-D np.array): Define the normals 
+            of the linear constraint hyperplanes. This is A^T in the constraints 
+            of the QP. Defaults to 0   
+        """
+        super().__init__(**kwargs)
+        shape = kwargs.get("shape", (1, 1))
+        self.s_in = InPort(shape=(shape[1],))      
+        self.a_out = OutPort(shape=(shape[0],))
+        self.weights = Var(shape=shape, 
+                           init=kwargs.pop("constraint_normals", 0)
+                           )
+
+class learningConstantAlpha(AbstractProcess):
+    """The learning constants that define the speed of convergence of the 
+    vanilla gradient descent
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Intialize the values of alpha, beta and the decay/growth schedules.
+        
+        Kwargs:
+            shape (int tuple): A tuple defining the shape of the connections 
+            matrix. Defaults to (1,1).
+            alpha (1-D np.array): Defines the learning rate for gradient 
+            descent. Defaults to 1.
+            alpha_decay_schedule: The number of iterations after which one right 
+            shift operation takes place for alpha. Default intialization 
+            to a very high value of 10000.
+        """
+        super().__init__(**kwargs)
+        shape = kwargs.get("shape", (1, 1))      
+        self.a_out = OutPort(shape=(shape[0],))
+        self.alpha = Var(shape=shape, 
+                         init=kwargs.pop("alpha", 1))                 
+        self.alpha_decay_schedule = Var(shape=shape, 
+                                        init=kwargs.pop("alpha_decay_schedule", 
+                                                        10000
+                                                        )
+                                        )  
+        self.decay_counter = Var(shape=shape, 
+                                  init=0)
+
+class learningConstantBeta(AbstractProcess):
+    """The learning constants that define the magnitude of correction from 
+    constraint checking
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Intialize the values of beta and the growth schedules.
+        
+        Kwargs:
+            shape (int tuple): A tuple defining the shape of the connections 
+            matrix. Defaults to (1,1).
+            beta (1-D np.array): Defines the learning rate for constraint-
+            checking. Defaults to 1.
+            beta_growth_schedule: The number of iterations after which one left 
+            shift operation takes place for beta. Default intialization 
+            to a very high value of 10000. 
+        """
+        super().__init__(**kwargs)
+        shape = kwargs.get("shape", (1, 1))      
+        self.a_out = OutPort(shape=(shape[0],))                
+        self.beta = Var(shape=shape, 
+                        init=kwargs.pop("beta", 1))
+        self.beta_growth_schedule = Var(shape=shape, 
+                                        init=kwargs.pop("beta_growth_schedule", 
+                                                        10000
+                                                        )
+                                        )
+        self.growth_counter = Var(shape=shape, 
+                                  init=0)

6594e3c2b3e1333a19eb2a5d71575ade42ed1545
Ashish <ashish.rao.mangalore@intel.com>
2021-11-04 14:30:35 +0100

    completed constraintCheck and GradientDynamics Heirarchial processes. Need to test them with the developing solver interface

--- lava/lib/optimization/solvers/qp_solver/models.py
+++ lava/lib/optimization/solvers/qp_solver/models.py
@@ -3,12 +3,14 @@
 # See: https://spdx.org/licenses/
 
 import numpy as np
+
 from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
 from lava.magma.core.model.py.ports import PyInPort, PyOutPort
 from lava.magma.core.model.py.type import LavaPyType
 from lava.magma.core.resources import CPU
 from lava.magma.core.decorator import implements, requires
 from lava.magma.core.model.py.model import PyLoihiProcessModel
+from lava.magma.core.model.sub.model import AbstractSubProcessModel
 from processes import *
 
 
@@ -31,12 +33,12 @@ class PyDenseModel(PyLoihiProcessModel):
 class PyDenseModel(PyLoihiProcessModel):
     s_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.int32, precision=24)
     a_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, precision=24)
-    threshold: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+    thresholds: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
 
     def run_spk(self):
         s_in = self.s_in.recv()
         # constraint violation check
-        a_out = (s_in-self.threshold)*(s_in < self.threshold)
+        a_out = (s_in-self.thresholds)*(s_in < self.thresholds)
         self.a_out.send(a_out)
         self.a_out.flush()
 
@@ -61,21 +63,16 @@ class PyDenseModel(PyLoihiProcessModel):
     a_out_qc: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, 
                                      precision=24)
     s_in_cn: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.int32, precision=24)
-    a_out_cn: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, 
-                                     precision=24)
     a_out_cc: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, 
                                      precision=24)
     qp_neuron_state: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
     grad_bias: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
-    
     alpha: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
     beta: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
-    alpha_decay_schedule: np.ndarray = LavaPyType(np.ndarray, np.int32, 
-                                                  precision=24)
-    beta_growth_schedule: np.ndarray = LavaPyType(np.ndarray, np.int32, 
-                                                  precision=24)
-    decay_counter: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
-    growth_counter: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+    alpha_decay_schedule: int = LavaPyType(int, np.int32, precision=24)
+    beta_growth_schedule: int = LavaPyType(int, np.int32, precision=24)
+    decay_counter: int = LavaPyType(int, np.int32, precision=24)
+    growth_counter: int = LavaPyType(int, np.int32, precision=24)
 
     def run_spk(self):
         s_in_qc = self.s_in_qc.recv()
@@ -97,15 +94,12 @@ class PyDenseModel(PyLoihiProcessModel):
                                 -self.beta*s_in_cn
         # Perhaps reduce the number of OutPorts if behaviour is consistent, 
         # to mimic actual hardware microcode implementation
-        a_out_cn = self.qp_neuron_state
         a_out_qc = self.qp_neuron_state
         a_out_cc = self.qp_neuron_state
 
         self.a_out_qc.send(a_out_qc)
-        self.a_out_cn.send(a_out_cn)
         self.a_out_cc.send(a_out_cc)
         self.a_out_qc.flush()
-        self.a_out_cn.flush()
         self.a_out_cc.flush()
 
 @implements(proc=constraintNormals, protocol=LoihiProtocol)
@@ -121,3 +115,95 @@ class PyDenseModel(PyLoihiProcessModel):
         a_out = self.weights@s_in
         self.a_out.send(a_out)
         self.a_out.flush()
+
+@implements(proc=constraintCheck, protocol=LoihiProtocol)
+class SubDenseLayerModel(AbstractSubProcessModel):
+    """Implement constraintCheckProcess behavior via sub Processes."""
+    s_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.int32, precision=24)
+    constraint_matrix: np.ndarray = LavaPyType(np.ndarray, np.int32, 
+                                               precision=8)
+    constraint_bias: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+    a_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, precision=24)
+
+    def __init__(self, proc):
+        """Builds sub Process structure of the Process."""
+        constraint_matrix = proc.init_args.get("constraint_matrix", 0)
+        constraint_bias = proc.init_args.get("constraint_bias", 0)
+        
+        # Initialize subprocesses
+        self.constraintDirections = constraintDirections(
+                                    shape=constraint_matrix.shape, 
+                                    weights=constraint_matrix
+                                    )
+        self.constraintNeurons = constraintNeurons(shape=constraint_bias.shape,
+                                                   thresholds=constraint_bias)
+        
+        # connect subprocesses
+        proc.in_ports.s_in.connect(self.constraintDirections.in_ports.s_in)
+        self.constraintDirections.out_ports.a_out.connect(
+                                        self.constraintNeurons.in_ports.s_in)
+        self.constraintNeurons.out_ports.s_out.connect(proc.out_ports.s_out)
+        
+        #alias process variables to subprocess variables
+        proc.vars.constraint_matrix.alias(
+            self.constraintDirections.vars.weights)
+
+        proc.vars.constraint_bias.alias(self.constraintNeurons.vars.thresholds)
+
+
+@implements(proc=gradientDynamics, protocol=LoihiProtocol)
+class SubDenseLayerModel(AbstractSubProcessModel):
+    """Implement gradientDynamics Process behavior via sub Processes."""
+    s_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.int32, precision=24)
+    hessian: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=8)
+    constraint_matrix_T: np.ndarray = LavaPyType(np.ndarray, np.int32, 
+                                               precision=8)
+    grad_bias: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+    qp_neuron_state: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+    alpha: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+    beta: np.ndarray = LavaPyType(np.ndarray, np.int32, precision=24)
+    alpha_decay_schedule: int = LavaPyType(int, np.int32, precision=24)
+    beta_growth_schedule: int = LavaPyType(int, np.int32, precision=24)
+    a_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.int32, precision=24)
+
+    def __init__(self, proc):
+        """Builds sub Process structure of the Process."""
+        hessian = proc.init_args.get("hessian", 0)
+        shape_hess = hessian.shape
+        shape_sol = (shape_hess[0], 1)
+        A_T =  proc.init_args.get("constraint_matrix_T", 0)
+        shape_A_T = A_T.shape
+        grad_bias = proc.init_args.get("grad_bias", 
+                                        np.zeros(shape_sol)
+                                      ) 
+        qp_neuron_state = proc.init_args.get("qp_neurons_init", 
+                                              np.zeros(shape_sol)
+                                            )
+        alpha = proc.init_args.get("alpha", np.ones(shape_sol))
+        beta = proc.init_args.get("beta", np.ones(shape_sol))
+        alpha_decay_schedule = proc.init_args.get("alpha_decay_schedule", 10000)
+        beta_growth_schedule = proc.init_args.get("beta_decay_schedule", 10000)
+
+        # Initialize subprocesses
+        self.qC = quadraticConnectivity(shape_hess, hessian)
+        self.sN = solutionNeurons(shape_sol, qp_neuron_state, grad_bias, alpha, 
+                                  beta, alpha_decay_schedule, 
+                                  beta_growth_schedule)                           
+        self.cN = constraintNormals(shape_A_T, A_T)
+
+        # connect subprocesses
+        proc.in_ports.s_in.connect(self.cN.in_ports.s_in)  
+        self.cN.out_ports.a_out.connect(self.sN.in_ports.s_in_cn)
+        self.qC.out_ports.a_out.connect(self.sN.in_ports.s_in_qc)
+        self.sN.out_ports.a_out_qc.connect(self.qC.in_ports.s_in)
+        self.sN.out_ports.a_out_cc.connect(proc.out_ports.a_out)
+
+        #alias process variables to subprocess variables
+        proc.vars.hessian.alias(self.qC.vars.weights)
+        proc.vars.constraint_matrix_T.alias(self.cN.vars.weights)
+        proc.vars.grad_bias.alias(self.sN.vars.grad_bias)
+        proc.vars.qp_neuron_state.alias(self.sN.vars.qp_neuron_state)
+        proc.vars.alpha.alias(self.sN.vars.alpha)
+        proc.vars.beta.alias(self.sN.vars.beta)
+        proc.vars.alpha_decay_schedule.alias(self.sN.vars.alpha_decay_schedule)
+        proc.vars.beta_growth_schedule.alias(self.sN.vars.beta_growth_schedule)

5d3d04eedaecd668d53530c1dc2b01b95c146230
GaboFGuerra <gabriel.guerra@intel.com>
2021-11-07 14:02:24 -0800

    Add Abstract OptimizationProblem class.

--- src/lava/lib/optimization/problems/problems.py
+++ src/lava/lib/optimization/problems/problems.py
@@ -1,3 +1,12 @@
 # Copyright (C) 2021 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 # See: https://spdx.org/licenses/
+
+
+class OptimizationProblem:
+    """Abstract optimization problem from which the actual problems derive.
+    """
+
+    def __init__(self, cost=None, constraints=None):
+        self.cost = cost
+        self.constraints = constraints

101e9ec21d9426ceb8f9eb1c9e29acedfc71a730
GaboFGuerra <gabriel.fonseca.guerra@intel.com>
2022-01-24 20:24:13 +0100

    Create DiscreteConstraints class that passes tests.
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>

---
+++ src/lava/lib/optimization/problems/constraints.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+# See: https://spdx.org/licenses/
+import typing as ty
+import numpy.typing as npt
+
+class DiscreteConstraints:
+    """Constraint involving only discrete variables.
+
+    :param relations: a binary tensor indicating what values of the variables
+    related by this constraint are allowed.
+    """
+
+    def __init__(self, constraints: ty.List[ty.Tuple[int, int, npt.ArrayLike]]):
+        self._constraints =constraints
+        var_subsets, relations = self.get_scope_and_relations(constraints)
+        self.validate_subsets_and_relations_match(var_subsets, relations)
+        self._var_subset = var_subsets
+        self._relations = relations
+
+    @property
+    def relations(self):
+        return self._relations
+
+    @property
+    def var_subsets(self):
+        return self._var_subset
+
+    def get_scope_and_relations(self, constraints):
+        var_subsets = [tuple(c[:-1]) for c in constraints]
+        relations = [c[-1] for c in constraints]
+        return var_subsets, relations
+
+    def validate_subsets_and_relations_match(self, subsets, relations):
+        for subset, relation in zip(subsets, relations):
+            assert len(subset) ==  relation.ndim

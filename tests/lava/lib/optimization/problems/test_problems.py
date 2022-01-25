c659ad7f6210d3d21da34593f697e7b093134023
GaboFGuerra <gabriel.fonseca.guerra@intel.com>
2022-01-25 09:02:36 +0100

    Include failing unit tests for a QUBO problem definition.
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>

--- tests/lava/lib/optimization/problems/test_problems.py
+++ tests/lava/lib/optimization/problems/test_problems.py
@@ -4,9 +4,13 @@
 
 import unittest
 
+import numpy as np
 from src.lava.lib.optimization.problems.constraints import Constraints
-from src.lava.lib.optimization.problems.problems import OptimizationProblem
-from src.lava.lib.optimization.problems.variables import Variables
+from src.lava.lib.optimization.problems.cost import Cost
+from src.lava.lib.optimization.problems.problems import (OptimizationProblem,
+                                                         QUBO)
+from src.lava.lib.optimization.problems.variables import (Variables,
+                                                          DiscreteVariables)
 
 
 class CompliantInterfaceInheritance(OptimizationProblem):
@@ -53,6 +57,67 @@ class TestOptimizationProblem(unittest.TestCase):
     def test_constraints_attribute(self):
         self.assertIsInstance(self.compliant_instantiation._constraints,
                               Constraints)
+
+
+class TestQUBO(unittest.TestCase):
+    def setUp(self) -> None:
+        self.qubo = QUBO(np.eye(10))
+
+    def test_create_obj(self):
+        self.assertIsInstance(self.qubo, QUBO)
+
+    def test_q_is_square_matrix(self):
+        n, m = self.qubo.cost.coefficients[2].shape
+        self.assertEqual(n, m)
+
+    def test_cost_class(self):
+        self.assertIsInstance(self.qubo.cost, Cost)
+
+    def test_only_quadratic_term_in_cost(self):
+        self.assertEqual(list(self.qubo.cost.coefficients.keys()), [2])
+
+    def test_variables_class(self):
+        self.assertIsInstance(self.qubo.variables, DiscreteVariables)
+
+    def test_variables_are_binary(self):
+        for n, size in enumerate(self.qubo.variables.domain_sizes):
+            with self.subTest(msg=f'Var ID {n}'):
+                self.assertEqual(size, 2)
+
+    def test_constraints_is_none(self):
+        self.assertIsNone(self.qubo.constraints)
+
+    def test_cannot_set_constraints(self):
+        with self.assertRaises(AttributeError):
+            self.qubo.constraints = np.eye(10)
+
+    def test_set_cost(self):
+        new_cost = np.eye(10)
+        self.qubo.cost = new_cost
+        self.assertIs(self.qubo.cost.get_coefficient(2), new_cost)
+
+    def test_class_of_setted_cost(self):
+        new_cost = np.eye(10)
+        self.qubo.cost = new_cost
+        self.assertIsInstance(self.qubo.cost, Cost)
+
+    def test_cannot_set_nonquadratic_cost(self):
+        with self.assertRaises(AssertionError):
+            self.qubo.cost = np.eye(10).reshape(5, 20)
+
+    def test_assertion_raised_if_q_is_not_square(self):
+        with self.assertRaises(AssertionError):
+            QUBO(np.eye(10).reshape(5, 20))
+
+    def test_validate_input_method_fails_assertion(self):
+        with self.assertRaises(AssertionError):
+            self.qubo.validate_input(np.eye(10).reshape(5, 20))
+
+    def test_validate_input_method_does_not_fail_assertion(self):
+        try:
+            self.qubo.validate_input(np.eye(10))
+        except AssertionError as ex:
+            self.fail("Assertion failed with correct input!")
 
 
 if __name__ == '__main__':

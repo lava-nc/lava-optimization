1f779d4513a9527404da124f915148c5c79289c4
GaboFGuerra <gabriel.fonseca.guerra@intel.com>
2022-08-29 09:47:12 +0200

    Create auxiliary function that creates Vars from tensor coefficients.
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>

--- src/lava/lib/optimization/solvers/generic/processes.py
+++ src/lava/lib/optimization/solvers/generic/processes.py
@@ -15,6 +15,22 @@ from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
 from lava.lib.optimization.problems.coefficients import CoefficientTensorsMixin
 from lava.lib.optimization.problems.problems import OptimizationProblem
 
+def _vars_from_coefficients(coefficients: CoefficientTensorsMixin) -> \
+        ty.Dict[int, AbstractProcessMember]:
+    vars = dict()
+    for rank, coeff in coefficients.items():
+        if rank == 1:
+            init=-coeff
+        if rank == 2:
+            linear_component = -coeff.diagonal()
+            quadratic_component = coeff*np.logical_not(np.eye(*coeff.shape))
+            if 1 in  vars.keys():
+                vars[1].init = vars[1].init +  linear_component
+            else:
+                vars[1] = Var(shape=linear_component.shape,
+                              init=linear_component)
+            init = -quadratic_component
+        vars[rank] = Var(shape=coeff.shape, init=init)
 
 def _in_ports_from_coefficients(coefficients: CoefficientTensorsMixin) -> \
         ty.List[AbstractProcessMember]:

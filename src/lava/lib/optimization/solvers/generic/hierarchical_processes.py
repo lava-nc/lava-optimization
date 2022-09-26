99cc9daf0e65b1457c382466f59ce393ac2aeae1
GaboFGuerra <gabriel.fonseca.guerra@intel.com>
2022-09-26 13:42:59 +0200

    Squash commit bringing changes from private development.
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>

---
+++ src/lava/lib/optimization/solvers/generic/hierarchical_processes.py
@@ -0,0 +1,251 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+# See: https://spdx.org/licenses/
+import typing as ty
+
+import numpy as np
+from lava.magma.core.process.ports.ports import InPort, OutPort
+from lava.magma.core.process.process import AbstractProcess, LogConfig
+from lava.magma.core.process.variable import Var
+from numpy import typing as npt
+
+
+class ContinuousVariablesProcess(AbstractProcess):
+    """Process which implementation holds the evolution of continuous
+    variables on the solver of an optimization problem."""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        raise NotImplementedError
+
+
+class DiscreteVariablesProcess(AbstractProcess):
+    """Process which implementation holds the evolution of discrete variables
+    on the solver of an optimization problem.
+
+    Parameters
+    ----------
+    shape: a tuple of the form (number of variables, domain size).
+    step_size: The coefficient of the variables indicating their relative
+    linear contribution to the cost function. Usually corresponds to the linear
+    term or the diagonal of the quadratic term on the cost function.
+    cost_diagonal: The diagonal of the coefficient of the quadratic term on the
+    cost function.
+    name: Name of the Process. Default is 'Process_ID', where ID is an
+    integer value that is determined automatically.
+    log_config: Configuration options for logging.z
+
+    InPorts
+    -------
+    a_in: The addition of all inputs (per dynamical system) at this timestep
+    will be received by this port.
+
+    OutPorts
+    --------
+    s_out: The payload to be exchanged between the underlying dynamical systems
+    when these fire.
+    local_cost: the cost components per dynamical system underlying these
+    variables, i.e., c_i = sum_j{Q_{ij} \cdot x_i}  will be sent through this
+    port. The cost integrator will then complete the cost computation  by
+    adding all contributions, i.e., x^T \cdot Q \cdot x = sum_i{c_i}.
+
+    Vars
+    ----
+    variable_assignment: Holds the current value assigned to the variables by
+    the solver network.
+    """
+
+    def __init__(self, shape: ty.Tuple[int, ...],
+                 cost_diagonal: npt.ArrayLike = None,
+                 hyperparameters: ty.Dict[str, ty.Union[int,
+                                                        npt.ArrayLike]] = None,
+                 name: ty.Optional[str] = None,
+                 log_config: ty.Optional[LogConfig] = None) -> None:
+        super().__init__(shape=shape,
+                         cost_diagonal=cost_diagonal,
+                         name=name,
+                         log_config=log_config)
+        self.num_variables = np.prod(shape)
+        self.hyperparameters = hyperparameters
+        self.a_in = InPort(shape=shape)
+        self.s_out = OutPort(shape=shape)
+        self.variable_assignment = Var(shape=shape)
+        self.local_cost = OutPort(shape=shape)
+
+
+class CostConvergenceChecker(AbstractProcess):
+    """Process that continuously monitors cost convergence.
+
+    Parameters
+    ----------
+    shape: The expected shape of the input cost components.
+    name: Name of the Process. Default is 'Process_ID', where ID is an
+    integer value that is determined automatically.
+    log_config: Configuration options for logging.
+
+
+    InPorts
+    -------
+    cost_components: Additive contributions to the total cost.
+
+    OutPorts
+    --------
+    update_buffer: OutPort which notifies the next process about the
+    detection of a better cost.
+
+    Vars
+    ----
+    min_cost: Current minimum cost, i.e., the lowest reported cost so far.
+
+    """
+
+    def __init__(self,
+                 shape: ty.Tuple[int, ...],
+                 name: ty.Optional[str] = None,
+                 log_config: ty.Optional[LogConfig] = None, ) -> None:
+        super().__init__(shape=shape,
+                         name=name,
+                         log_config=log_config)
+        self.shape = shape
+        self.min_cost = Var(shape=(1,))
+        self.cost_components = InPort(shape=shape)
+        self.update_buffer = OutPort(shape=(1,))
+
+
+class SatConvergenceChecker(AbstractProcess):
+    """Process that continuously monitors satisfiability convergence."""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.satisfaction = None
+        raise NotImplementedError
+
+
+class AugmentedTermsProcess(AbstractProcess):
+    """Process implementing cost coefficients as synapses."""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        raise NotImplementedError
+
+
+class ContinuousConstraintsProcess(AbstractProcess):
+    """Process implementing continuous constraints via neurons and synapses."""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        raise NotImplementedError
+
+
+class DiscreteConstraintsProcess(AbstractProcess):
+    """Process implementing discrete constraints via synapses."""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        raise NotImplementedError
+
+
+class MixedConstraintsProcess(AbstractProcess):
+    """Process implementing continuous constraints via neurons and synapses."""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        raise NotImplementedError
+
+
+class StochasticIntegrateAndFire(AbstractProcess):
+    """Event-driven stochastic discrete dynamical system with two outputs.
+
+    The main output is intended as input to other dynamical systems on
+    the network, whilst the second output is to transfer local information to be
+    integrated by an auxiliary dynamical system or circuit.
+
+    Parameters
+    ----------
+    shape: The shape of the set of dynamical systems to be created.
+    init_state: The starting value of the state variable.
+    step_size: a value to be added to the state variable at each timestep.
+    noise_amplitude: The width/range for the stochastic perturbation to the
+    state variable. A random number within this range will be added to the
+    state variable at each timestep.
+    input_duration: Number of timesteps by which each input should be preserved.
+    min_state: The minimum value for the state variable. The state variable
+    will be truncated at this value if updating results in a lower value.
+    min_integration: The minimum value for the total input (addition of all
+    valid inputs at a given timestep). The total input value will be truncated
+    at this value if adding current and preserved inputs results in a lower
+    value.
+    steps_to_fire: After how many timesteps would the dynamical system fire
+    and reset without stochastic perturbation. Note that if noise_amplitude > 0,
+    the system will stochastically deviate from this value.
+    refractory_period: number of timesteps to wait after firing and reset before
+    resuming updating.
+    cost_diagonal: The linear coefficients on the cost function of the
+    optimization problem where this system will be used.
+    name: Name of the Process. Default is 'Process_ID', where ID is an
+    integer value that is determined automatically.
+    log_config: Configuration options for logging.
+
+    InPorts
+    -------
+    added_input: The addition of all inputs (per dynamical system) at this
+    timestep will be received by this port.
+    replace_assignment: Todo: deprecate
+
+    OutPorts
+    --------
+    messages: The payload to be sent to other dynamical systems when firing.
+    local_cost: the cost component corresponding to this dynamical system, i.e.,
+    c_i = sum_j{Q_{ij} \cdot x_i}  will be sent through this port. The cost
+    integrator will then complete the cost computation  by adding all
+    contributions, i.e., x^T \cdot Q \cdot x = sum_i{c_i}.
+
+    """
+
+    def __init__(self, *,
+                 step_size: npt.ArrayLike,
+                 shape: ty.Tuple[int, ...] = (1,),
+                 init_state: npt.ArrayLike = 0,
+                 noise_amplitude: npt.ArrayLike = 0,
+                 input_duration: npt.ArrayLike = 6,
+                 min_state: npt.ArrayLike = 1000,
+                 min_integration: npt.ArrayLike = -1000,
+                 steps_to_fire: npt.ArrayLike = 10,
+                 refractory_period: npt.ArrayLike = 1,
+                 cost_diagonal: npt.ArrayLike = 0,
+                 name: ty.Optional[str] = None,
+                 log_config: ty.Optional[LogConfig] = None,
+                 init_value: None = 0) -> None:
+        super().__init__(
+            shape=shape,
+            initial_state=init_state,
+            step_size=step_size,
+            noise_amplitude=noise_amplitude,
+            input_duration=input_duration,
+            min_state=min_state,
+            min_integration=min_integration,
+            steps_to_fire=steps_to_fire,
+            refractory_period=refractory_period,
+            cost_diagonal=cost_diagonal,
+            name=name,
+            log_config=log_config,
+            init_value=init_value
+        )
+        self.added_input = InPort(shape=shape)
+        self.messages = OutPort(shape=shape)
+        self.local_cost = OutPort(shape=shape)
+
+        self.integration = Var(shape=shape, init=0)
+        self.step_size = Var(shape=shape, init=step_size)
+        self.state = Var(shape=shape, init=init_state)
+        self.noise_amplitude = Var(shape=shape, init=noise_amplitude)
+        self.input_duration = Var(shape=shape, init=input_duration)
+        self.min_state = Var(shape=shape, init=min_state)
+        self.min_integration = Var(shape=shape, init=min_integration)
+        self.steps_to_fire = Var(shape=shape, init=steps_to_fire)
+        self.refractory_period = Var(shape=shape, init=refractory_period)
+        self.firing = Var(shape=shape, init=init_value)
+        self.prev_assignment = Var(shape=shape, init=False)
+        self.cost_diagonal = Var(shape=shape, init=cost_diagonal)
+        self.assignment = Var(shape=shape, init=False)
+        self.min_cost = Var(shape=shape, init=False)
--- src/lava/lib/optimization/solvers/generic/solver.py
+++ src/lava/lib/optimization/solvers/generic/solver.py
@@ -1,36 +1,91 @@
 # Copyright (C) 2021 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 # See: https://spdx.org/licenses/
-from lava.magma.core.run_conditions import RunSteps, RunContinuous
-from lava.magma.core.run_configs import Loihi1SimCfg
+import typing as ty
 
+import numpy.typing as npt
 from lava.lib.optimization.problems.problems import OptimizationProblem
-from lava.lib.optimization.solvers.generic.processes import SolverProcessBuilder
-from lava.lib.optimization.solvers.generic.models import SolverModelBuilder
+from lava.lib.optimization.solvers.generic.builder import SolverProcessBuilder
+from lava.lib.optimization.solvers.generic.hierarchical_processes import \
+    StochasticIntegrateAndFire
+from lava.lib.optimization.solvers.generic.sub_process_models import \
+    StochasticIntegrateAndFireModel, StochasticIntegrateAndFireModelSCIF
+from lava.magma.core.resources import AbstractComputeResource, CPU, \
+    Loihi2NeuroCore, NeuroCore
+from lava.magma.core.run_conditions import RunContinuous, RunSteps
+from lava.magma.core.run_configs import Loihi1SimCfg, Loihi2HwCfg
+from lava.magma.core.sync.protocol import AbstractSyncProtocol
+from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
+from lava.proc.dense.models import PyDenseModelFloat
+from lava.proc.dense.ncmodels import NcModelDense
+from lava.proc.dense.process import Dense
+from lava.proc.read_gate.models import ReadGatePyModel
+from lava.proc.read_gate.ncmodels import ReadGateCModel
+from lava.proc.read_gate.process import ReadGate
+from lava.proc.scif.ncmodels import NcModelQuboScif
+from lava.proc.scif.process import QuboScif
+
+BACKENDS = ty.Union[CPU, Loihi2NeuroCore, NeuroCore, str]
+CPUS = [CPU, "CPU"]
+NEUROCORES = [Loihi2NeuroCore, NeuroCore, "Loihi2"]
 
 
-# from lava.utils.profiler import LavaProfiler
-class LavaProfiler:
-    # todo placeholder while profiler is implemented.
-    pass
-
-    def profile(self, process):
-        pass
-
-
-class OptimizationSolver:
-    """Wrapper over the actual Lava OptimizationSolverProcess.
+def solve(problem: OptimizationProblem,
+          timeout: int,
+          target_cost: int = None,
+          backend: BACKENDS = Loihi2NeuroCore) -> \
+        npt.ArrayLike:
+    """Create solver from problem spec and run until target_cost or timeout.
 
     Parameters
     ----------
-    configuration: Configuration parameters for the OptimizationProcessSolver.
+    problem: Optimization problem to be solved.
+    timeout: Maximum number of iterations (timesteps) to be run. If set to
+    -1 then the solver will run continuously in non-blocking mode until a
+     solution is found.
+    target_cost: A cost value provided by the user as a target for the
+    solution to be found by the solver, when a solution with such cost is
+    found and read, execution ends.
+    backend: Specifies the backend where the main solver network will be
+    deployed.
+
+    Returns
+    ----------
+    solution: candidate solution to the input optimization problem.
+    """
+    solver = OptimizationSolver(problem)
+    solution = solver.solve(timeout=timeout, target_cost=target_cost,
+                            backend=backend)
+    return solution
+
+
+class OptimizationSolver:
+    """Generic solver for constrained optimization problems defined by
+    variables, cost and constraints.
+
+    The problem should behave according to the OptimizationProblem's
+    interface so that the Lava solver can be built correctly.
+
+    A Lava OptimizationSolverProcess and a Lava OptimizationSolverModel will
+    be created from the problem specification. The dynamics of such process
+    implements the algorithms that search a solution to the problem and
+    reports it to the user.
+
+    Parameters
+    ----------
+    problem: Optimization problem to be solved.
+    run_cfg: Run configuration for the OptimizationSolverProcess.0
+
     """
 
-    def __init__(self, run_cfg=None):
+    def __init__(self,
+                 problem: OptimizationProblem,
+                 run_cfg=None):
+        self.problem = problem
         self._run_cfg = run_cfg
         self._process_builder = SolverProcessBuilder()
-        self._solver_process = None
-        self._solver_model = None
+        self.solver_process = None
+        self.solver_model = None
 
     @property
     def run_cfg(self):
@@ -41,63 +96,130 @@ class OptimizationSolver:
     def run_cfg(self, value):
         self._run_cfg = value
 
-    def solve(
-        self,
-        problem: OptimizationProblem,
-        timeout: int,
-        target_cost: int = None,
-        profiling: bool = False,
-    ):
-        """Create solver from problem specs and run until solution or timeout.
+    def solve(self,
+              timeout: int,
+              target_cost: int = 0,
+              backend: BACKENDS = CPU,
+              hyperparameters: ty.Dict[
+                  str, ty.Union[int, npt.ArrayLike]] = None) \
+            -> npt.ArrayLike:
+        """Create solver from problem spec and run until target_cost or timeout.
 
         Parameters
         ----------
-        problem: Optimization problem to be solved.
-        timeout: Maximum number of iterations/timesteps to be run, if set to
+        timeout: Maximum number of iterations (timesteps) to be run. If set to
         -1 then the solver will run continuously in non-blocking mode until a
          solution is found.
-        profiling: Whether to profile the run. This will measure or estimate
-        energy and time depending on the backend.
+        target_cost: A cost value provided by the user as a target for the
+        solution to be found by the solver, when a solution with such cost is
+        found and read, execution ends.
+        backend: Specifies the backend where the main solver network will be
+        deployed.
+        hyperparameters: A dictionary specifying values for steps_to_fire,
+        noise_amplitude, step_size and init_value. All but the last are
+        integers, the initial value is an array-like of initial values for the
+        variables defining the problem.
 
         Returns
         ----------
         solution: candidate solution to the input optimization problem.
 
         """
-        solver_process = self._create_solver_process(problem)
-        solver_model = self._create_solver_model(solver_process, target_cost)
-        if profiling:
-            profiler = LavaProfiler()
-            solver_process = profiler.profile(solver_process)
-        pdict = {solver_process: solver_model}
-        solver_process.run(
+        run_cfg = None
+        if not self.solver_process:
+            self._create_solver_process(self.problem, target_cost, backend,
+                                        hyperparameters)
+        if backend in CPUS:
+            pdict = {self.solver_process: self.solver_model,
+                     ReadGate: ReadGatePyModel,
+                     Dense: PyDenseModelFloat,
+                     StochasticIntegrateAndFire: StochasticIntegrateAndFireModel
+                     }
+            run_cfg = Loihi1SimCfg(exception_proc_model_map=pdict)
+        elif backend in NEUROCORES:
+            pdict = {self.solver_process: self.solver_model,
+                     ReadGate: ReadGateCModel,
+                     Dense: NcModelDense,
+                     StochasticIntegrateAndFire:
+                         StochasticIntegrateAndFireModelSCIF,
+                     QuboScif: NcModelQuboScif
+                     }
+            run_cfg = Loihi2HwCfg(exception_proc_model_map=pdict)
+        else:
+            raise NotImplementedError(str(backend) + backend_msg)
+        self.solver_process._log_config.level = 20
+        self.solver_process.run(
             condition=RunContinuous()
             if timeout == -1
             else RunSteps(num_steps=timeout),
-            run_cfg=Loihi1SimCfg(  # select_sub_proc_model=True,
-                exception_proc_model_map=pdict
-            ),
+            run_cfg=run_cfg,
         )
         if timeout == -1:
-            solver_process.wait()
-        solution = solver_process.variable_assignment.aliased_var.get()
-        solver_process.stop()
+            self.solver_process.wait()
+        solution = self.solver_process.variable_assignment.aliased_var.get()
+        self.solver_process.stop()
         return solution
 
-    def _create_solver_process(self, problem):
-        self._process_builder.create_constructor(problem)
-        self._solver_process = self._process_builder.solver_process
-        return self._solver_process
+    def _create_solver_process(self,
+                               problem: OptimizationProblem,
+                               target_cost: ty.Optional[int] = None,
+                               backend: BACKENDS = None,
+                               hyperparameters: ty.Dict[str,
+                                                        ty.Union[
+                                                            int, npt.ArrayLike]] = None):
+        """Create process and model class as solver for the given problem.
 
-    def _create_solver_model(self, solver_process, target_cost):
-        model_builder = SolverModelBuilder(solver_process)
-        model_builder.create_constructor(target_cost)
-        self._solver_model = model_builder.solver_model
-        return self._solver_model
+        Parameters
+        ----------
+        problem: Optimization problem defined by cost and constraints which
+        will be used to build the process and its model.
+        target_cost: A cost value provided by the user as a target for the
+        solution to be found by the solver, when a solution with such cost is
+        found and read, execution ends.
+        backend: Specifies the backend where the main solver network will be
+        deployed.
+        """
+        requirements, protocol = self._get_requirements_and_protocol(backend)
+        self._process_builder.create_solver_process(problem, hyperparameters
+                                                    or dict())
+        self._process_builder.create_solver_model(target_cost,
+                                                  requirements,
+                                                  protocol)
+        self.solver_process = self._process_builder.solver_process
+        self.solver_model = self._process_builder.solver_model
 
-    @classmethod
-    def get_process(cls, spec=None):
-        return
+    def _get_requirements_and_protocol(self,
+                                       backend: BACKENDS) -> \
+            ty.Tuple[
+                AbstractComputeResource, AbstractSyncProtocol]:
+        """Figure out requirements and protocol for a given backend.
 
-    def verify_solution(self, solution):
-        pass
+        Parameters
+        ----------
+        backend: Specifies the backend for which requirements and protocol
+        classes will be returned.
+
+        """
+        protocol = LoihiProtocol
+        if backend in CPUS:
+            return [CPU], protocol
+        elif backend in NEUROCORES:
+            return [Loihi2NeuroCore], protocol
+        else:
+            raise NotImplementedError(str(backend) + backend_msg)
+
+
+# TODO throw an error if L2 is not present and the user tries to use it.
+backend_msg = f""" was requested as backend. However,
+the solver currently supports only Loihi 2 and CPU backends. 
+These can be specified by calling solve with any of the following:
+
+    backend = "CPU"
+    backend = "Loihi2"
+    backend = CPU
+    backend = Loihi2NeuroCore
+    backend = NeuroCore
+
+The explicit resource classes can be imported from
+lava.magma.core.resources
+"""

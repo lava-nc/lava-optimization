09e96561953176890ea21a1c0591577d5f885ee8
GaboFGuerra <gabriel.fonseca.guerra@intel.com>
2022-09-26 20:45:34 +0200

    Make Loihi2 tests pass except for those requiring  workload solution.
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>

--- src/lava/lib/optimization/solvers/generic/sub_process_models.py
+++ src/lava/lib/optimization/solvers/generic/sub_process_models.py
@@ -30,7 +30,7 @@ from lava.magma.core.resources import CPU
 from lava.magma.core.decorator import implements, requires, tag
 from lava.magma.core.model.py.model import PyLoihiProcessModel
 from lava.proc.lif.process import LIF, TernaryLIF
-
+from lava.proc.scif.process import QuboScif
 
 @implements(proc=DiscreteVariablesProcess, protocol=LoihiProtocol)
 @requires(CPU)

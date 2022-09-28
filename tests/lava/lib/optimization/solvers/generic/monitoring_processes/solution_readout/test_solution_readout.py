dd9e07bdea64537f8993ee6ad73f36856d78a6da
GaboFGuerra <83413252+GaboFGuerra@users.noreply.github.com>
2022-09-28 02:50:35 +0200

    Update test parameters.

--- tests/lava/lib/optimization/solvers/generic/monitoring_processes/solution_readout/test_solution_readout.py
+++ tests/lava/lib/optimization/solvers/generic/monitoring_processes/solution_readout/test_solution_readout.py
@@ -20,9 +20,9 @@ class TestSolutionReadout(unittest.TestCase):
     def setUp(self) -> None:
         # Create processes.
         spiker = Spiker(shape=(4,), period=3, payload=7)
-        integrator = Spiker(shape=(1,), period=7, payload=4)
-        readgate = ReadGate(shape=(4,), target_cost=5)
-        self.readout = SolutionReadout(shape=(4,), target_cost=5)
+        integrator = Spiker(shape=(1,), period=7, payload=-4)
+        readgate = ReadGate(shape=(4,), target_cost=-3)
+        self.readout = SolutionReadout(shape=(4,), target_cost=-3)
 
         # Connect processes.
         integrator.s_out.connect(readgate.cost_in)
@@ -35,14 +35,9 @@ class TestSolutionReadout(unittest.TestCase):
         self.run_cfg = Loihi2HwCfg(exception_proc_model_map=pdict)
         self.readout._log_config.level = 20
 
-    def test_runs_without_stalling(self):
-        self.readout.run(RunSteps(num_steps=20), run_cfg=self.run_cfg)
-        self.readout.stop()
-
     def test_stops_when_desired_cost_reached(self):
-        self.readout.target_cost.init = 5
-        self.readout._log_config.level = 20
         self.readout.run(RunContinuous(), run_cfg=self.run_cfg)
         self.readout.wait()
+        solution = self.readout.solution.get()
         self.readout.stop()
-
+        self.assertTrue(np.all(solution == 2*np.ones(4)))

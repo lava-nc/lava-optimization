441b994bdeb4f5e05405b56be4026c36dee6d161
Pierro, Alessandro <alessandro.pierro@intel.com>
2022-10-24 00:41:38 -0700

    Refactor SolverBenchmarker

--- tests/lava/lib/optimization/utils/test_solver_benchmarker.py
+++ tests/lava/lib/optimization/utils/test_solver_benchmarker.py
@@ -24,41 +24,47 @@ class TestSolverBenchmarker(unittest.TestCase):
     def test_power_measurement_cfg(self):
         num_steps = 5
         power_cfg = self.benchmarker.get_power_measurement_cfg(
-            num_steps=num_steps)
+            num_steps=num_steps
+        )
         self.assertIsNotNone(power_cfg)
 
     def test_time_measurement_cfg(self):
         num_steps = 5
         time_cfg = self.benchmarker.get_time_measurement_cfg(
-            num_steps=num_steps)
+            num_steps=num_steps
+        )
         self.assertIsNotNone(time_cfg)
 
     def test_power_cfg(self):
         num_steps = 5
         power_cfg = self.benchmarker.get_power_measurement_cfg(
-            num_steps=num_steps)
+            num_steps=num_steps
+        )
         self.assertIsInstance(power_cfg[0][0], ty.Callable)
 
     def test_time_cfg(self):
         num_steps = 5
         time_cfg = self.benchmarker.get_time_measurement_cfg(
-            num_steps=num_steps)
+            num_steps=num_steps
+        )
         self.assertIsInstance(time_cfg[0][0], ty.Callable)
 
     def test_measured_power_property(self):
         num_steps = 5
         power_cfg = self.benchmarker.get_power_measurement_cfg(
-            num_steps=num_steps)
+            num_steps=num_steps
+        )
         measured_power = self.benchmarker.measured_power
         self.assertIsInstance(measured_power, np.ndarray)
 
     def test_measured_time_property(self):
         num_steps = 5
         time_cfg = self.benchmarker.get_time_measurement_cfg(
-            num_steps=num_steps)
+            num_steps=num_steps
+        )
         measured_time = self.benchmarker.measured_time
         self.assertIsInstance(measured_time, np.ndarray)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()

bbc1479ea4ce713431799f3d2fa6bb1ce9bd8f76
Pierro, Alessandro <alessandro.pierro@intel.com>
2022-10-26 18:39:34 +0200

    Address PR feedback

--- src/lava/lib/optimization/utils/solver_tuner.py
+++ src/lava/lib/optimization/utils/solver_tuner.py
@@ -11,13 +11,16 @@ import random
 class SolverTuner:
     """Class to find and set hyperparameters for an OptimizationSolver."""
 
-    def __init__(self,
-                 params_grid: ty.Dict
-                 ):
+    def __init__(self, params_grid: ty.Dict):
         """
         Instantiate a SolverTuner, defining the admissible search space.
 
-        :param params_grid:
+        Parameters
+        ----------
+        params_grid: dict
+            Dictionary where the keys are the name of the hyperparameters to be
+            tuned, and the values are tuples containing all the admissible
+            values for the associated hyperparameter.
         """
         self._params_grid = params_grid
 
@@ -27,26 +30,27 @@ class SolverTuner:
              stopping_condition: ty.Callable[[float, int], bool] = None):
         """Find and set solving hyperparameters for an OptimizationSolver"""
         # TODO : Check that hyperparameters are arguments for solver
-        best_hyperparameters, best_cost, best_step_to_sol = self._perform_grid_search(
+        best_hyperparameters, best_cost, best_step_to_sol = self._random_search(
             solver,
             solver_parameters,
             stopping_condition)
         succeeded = stopping_condition(best_cost, best_step_to_sol)
         return best_hyperparameters, succeeded
 
-    def _perform_grid_search(self,
-                             solver: OptimizationSolver,
-                             solver_parameters: ty.Dict,
-                             stopping_condition: ty.Callable[
-                                 [float, int], bool] = None
-                             ) -> ty.Union[ty.NoReturn, ty.Dict]:
+    def _random_search(self,
+                       solver: OptimizationSolver,
+                       solver_parameters: ty.Dict,
+                       stopping_condition: ty.Callable[
+                           [float, int], bool] = None
+                       ) -> ty.Union[ty.NoReturn, ty.Dict]:
 
         best_hyperparameters = None
         best_cost = float("inf")
         best_step_to_sol = float("inf")
         problem = solver.problem
         params_names = self._params_grid.keys()
-        params_grid = list(it.product(*map(lambda k: self._params_grid[k], params_names)))
+        params_grid = list(
+            it.product(*map(lambda k: self._params_grid[k], params_names)))
         random.shuffle(params_grid)
 
         for params in params_grid:
@@ -56,7 +60,8 @@ class SolverTuner:
             # TODO : Implement logic for CSP problems
             cost = solver.last_run_report["cost"]
             step_to_sol = solver.last_run_report["steps_to_solution"]
-            if cost is not None and cost <= best_cost and step_to_sol < best_step_to_sol:
+            if cost is not None and cost <= best_cost and (
+                    step_to_sol < best_step_to_sol):
                 best_hyperparameters = hyperparameters
                 best_cost = cost
                 best_step_to_sol = step_to_sol
@@ -64,8 +69,8 @@ class SolverTuner:
                         \nHyperparameters: {best_hyperparameters}\
                         \nBest cost: {best_cost}\
                         \nBest step-to-solution: {best_step_to_sol}""")
-            if stopping_condition is not None and stopping_condition(best_cost,
-                                                                     best_step_to_sol):
+            if stopping_condition is not None and (
+                    stopping_condition(best_cost, best_step_to_sol)):
                 break
             # TODO : Add internal logging
 

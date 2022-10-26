4cc3e883baf68747c0ac9a7bfdbf5b29f9108921
GaboFGuerra <83413252+GaboFGuerra@users.noreply.github.com>
2022-10-26 21:49:27 +0200

    Relocate optimization specific processes to lava-optimization and out of lava/proc (#83)
    * Add MISProblem class to represent a maximal independent set problem.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    * Add function to find maximum clique of an undirected graph.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    * Add function to find maximal independent set given and MISProblem instance.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    * Start unit test suite for MIS utilities.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    * Bring in optimization specific processes from lava/proc.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    * Address linter issues
    
    * Update import paths
    
    * Correct imports for SCIF processes on models file..
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    * Update licenses for scif tests.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    Co-authored-by: Pierro, Alessandro <alessandro.pierro@intel.com>

---
+++ tests/lava/lib/optimization/solvers/generic/scif/test_process.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+# See: https://spdx.org/licenses/
+import unittest
+import numpy as np
+from lava.lib.optimization.solvers.generic.scif.process import CspScif, QuboScif
+
+
+class TestCspScifProcess(unittest.TestCase):
+    """Tests for CspScif class"""
+    def test_init(self) -> None:
+        """Tests instantiation of CspScif"""
+        scif = CspScif(shape=(10,),
+                       step_size=2,
+                       theta=8,
+                       neg_tau_ref=-10)
+
+        self.assertEqual(scif.shape, (10,))
+        self.assertEqual(scif.step_size.init, 2)
+        self.assertEqual(scif.theta.init, 8)
+        self.assertEqual(scif.neg_tau_ref.init, -10)
+
+
+class TestQuboScifProcess(unittest.TestCase):
+    """Tests for QuboScif class"""
+    def test_init(self) -> None:
+        """Tests instantiation of QuboScif"""
+        scif = QuboScif(shape=(10,),
+                        step_size=2,
+                        theta=8,
+                        neg_tau_ref=-10,
+                        cost_diag=np.arange(1, 11))
+
+        self.assertEqual(scif.shape, (10,))
+        self.assertEqual(scif.step_size.init, 2)
+        self.assertEqual(scif.theta.init, 8)
+        self.assertEqual(scif.neg_tau_ref.init, -10)
+        self.assertTrue(np.all(scif.cost_diagonal.init == np.arange(1, 11)))

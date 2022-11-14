f457a83773ed5f19269e3be0fdc605672e383d1e
Alessandro Pierro <alessandro.pierro@intel.com>
2022-11-14 16:58:38 +0100

    Improve SolverTuner logic and unit tests (#123)
    * Update SolverTuner logic and tests
    
    * Fix numpy seeding for reproducibility
    
    * Update SolverTuner API
    
    * Address PR feedback, enabling redefinition of search space
    
    * Minor bug fix

--- tests/lava/lib/optimization/utils/test_solver_tuner.py
+++ tests/lava/lib/optimization/utils/test_solver_tuner.py
@@ -3,59 +3,123 @@
 # See: https://spdx.org/licenses/
 
 import unittest
-import typing as ty
 
 import numpy as np
 
-from lava.lib.optimization.problems.problems import QUBO, CSP
+from lava.lib.optimization.problems.problems import QUBO
 from lava.lib.optimization.solvers.generic.solver import OptimizationSolver
 from lava.lib.optimization.utils.solver_tuner import SolverTuner
 
 
-def prepare_problem_and_solver():
-    # Define the QUBO matrix
-    q = np.asarray([[-5, 2, 4, 0],
-                    [2, -3, 1, 0],
-                    [4, 1, -8, 5],
-                    [0, 0, 5, -6]])
+def prepare_solver_and_params():
+    """Generate an example QUBO workload."""
 
-    # Instantiate the QUBO problem
+    q = np.asarray([[-5, 2, 4, 0], [2, -3, 1, 0],
+                    [4, 1, -8, 5], [0, 0, 5, -6]])
     qubo_problem = QUBO(q=q)
 
-    # Instantiate a constraint optimization solver for this workload
     solver = OptimizationSolver(qubo_problem)
-    return solver
+    solver_params = {"timeout": 1000, "target_cost": -11, "backend": "CPU"}
+
+    return solver, solver_params
 
 
 class TestSolverTuner(unittest.TestCase):
+    """Unit tests for `SolverTuner` class."""
 
     def setUp(self) -> None:
-        params_grid = {
-            "step_size": (1, 2),
-            "noise_amplitude": (3, 4, 5, 6),
-        }
-        self.solver_tuner = SolverTuner(params_grid=params_grid)
+        """Setup test environment for `SolverTuner` class."""
+        self.search_space = [(1, 0), (1, 10), (20, 0), (20, 10)]
+        self.params_names = ['step_size', 'noise_amplitude']
+        self.shuffle = True
+        self.seed = 42
+        self.solver_tuner = SolverTuner(search_space=self.search_space,
+                                        params_names=self.params_names,
+                                        shuffle=self.shuffle,
+                                        seed=self.seed)
 
     def test_create_obj(self):
+        """Tests correct instantiation of `SolverTuner` object."""
         self.assertIsInstance(self.solver_tuner, SolverTuner)
 
+    def test_search_space_property(self):
+        """Tests `search_space` property exists and has the correct value."""
+        self.assertEqual(len(self.solver_tuner.search_space),
+                         len(self.search_space))
+        self.assertTrue(all(x in self.solver_tuner.search_space
+                            for x in self.search_space))
+
+    def test_params_names_property(self):
+        """Tests `params_names` property exists and has the correct value."""
+        self.assertEqual(len(self.solver_tuner.params_names),
+                         len(self.params_names))
+        self.assertTrue(all(x in self.solver_tuner.params_names
+                            for x in self.params_names))
+
+    def test_shuffle_property(self):
+        """Tests `shuffle` property exists and has the correct value."""
+        self.assertEqual(self.shuffle, self.solver_tuner.shuffle)
+
+    def test_seed_property(self):
+        """Tests `seed` property exists and has the correct value."""
+        self.assertEqual(self.seed, self.solver_tuner.seed)
+
+    def test_results_property(self):
+        """Tests `results` property exists and returns the correct type."""
+        self.assertIsInstance(self.solver_tuner.results, np.ndarray)
+
+    def test_shuffle_setter(self):
+        """Tests `shuffle` setter sets the correct value."""
+        self.solver_tuner.shuffle = not self.shuffle
+        self.assertEqual(self.solver_tuner.shuffle, not self.shuffle)
+
+    def test_seed_setter(self):
+        """Tests `seed` setter sets the correct value."""
+        self.solver_tuner.seed = self.seed + 1
+        self.assertEqual(self.solver_tuner.seed, self.seed + 1)
+
+    def test_search_space_setter(self):
+        """Tests `search_space` setter sets the correct value."""
+        self.solver_tuner.search_space = [self.search_space[0]]
+        self.assertEqual(self.solver_tuner.search_space, [self.search_space[0]])
+
+    def test_generate_grid_util(self):
+        """Tests `generate_grid` method produces the correct search space."""
+        params_domains = {
+            'step_size': (1, 20),
+            'noise_amplitude': (0, 10)
+        }
+        gen_search_space, gen_params_names = SolverTuner.generate_grid(
+            params_domains=params_domains
+        )
+        self.assertEqual(len(gen_search_space), len(self.search_space))
+        self.assertTrue(all(x in gen_search_space for x in self.search_space))
+        self.assertEqual(len(gen_params_names), len(self.params_names))
+        self.assertTrue(all(x in gen_params_names for x in self.params_names))
+
     def test_tune_success(self):
-        solver = prepare_problem_and_solver()
+        """Tests the correct set of hyper-parameters is found, for a known
+        problem."""
 
-        params = {"timeout": 1000,
-                  "target_cost": -11,
-                  "backend": "CPU"}
+        solver, solver_params = prepare_solver_and_params()
+        correct_best_hyperparams = {"step_size": 20, "noise_amplitude": 10}
 
-        def stop(best_cost, best_to_sol):
-            return best_cost < -6
+        def fitness(cost, step_to_sol):
+            return - step_to_sol if cost <= solver_params['target_cost'] \
+                else - float("inf")
 
-        hyperparams, success = self.solver_tuner.tune(solver=solver,
-                                                      solver_parameters=params,
-                                                      stopping_condition=stop)
+        fitness_target = -200
 
-        self.assertIsInstance(hyperparams, ty.Dict)
+        hyperparams, success = self.solver_tuner.tune(
+            solver=solver,
+            solver_params=solver_params,
+            fitness_fn=fitness,
+            fitness_target=fitness_target
+        )
+
+        self.assertEqual(hyperparams, correct_best_hyperparams)
         self.assertTrue(success)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()

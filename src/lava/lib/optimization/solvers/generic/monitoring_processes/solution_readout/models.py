e288e6eec5a65ff6370a0b1d78fd6cc18ef50fc8
Pierro, Alessandro <alessandro.pierro@intel.com>
2022-11-16 10:00:48 -0800

    Upate SolutionReadout

--- src/lava/lib/optimization/solvers/generic/monitoring_processes/solution_readout/models.py
+++ src/lava/lib/optimization/solvers/generic/monitoring_processes/solution_readout/models.py
@@ -38,37 +38,38 @@ class SolutionReadoutPyModel(PyAsyncProcessModel):
     def run_async(self):
         """Execute spiking phase, integrate input, update dynamics and send
         messages out."""
-        raw_cost = self.cost_in.recv()
-        self.acknowledgement.send(np.asarray([1]))
-        req_stop = self.req_stop_in.recv()
-        self.acknowledgement.send(np.asarray([1]))
-        cost = [0]
-        if raw_cost[0]:
-            # The following casts cost as a signed 24-bit value (8 = 32 - 24)
-            cost = (raw_cost.astype(np.int32) << 8) >> 8
-        if cost[0]:
-            raw_solution = self.read_solution.recv()
-            # ToDo: The following way of reading out solutions only works
-            #  when the solutions are binary, i.e., QUBO problems. It relies
-            #  on the assumption that `solution' is the spiking history of the
-            #  neurons solving a problem and picks the spiking history from 3
-            #  timesteps ago, when the minimum cost was actually achieved.
-            self.solution[:] = (raw_solution.astype(np.int8) >> 2) & 1
-            self.min_cost = cost[0]
-            if req_stop[0] != 0:
-                self.solution_step = req_stop[0]
-                print(f"Host: received a better solution: "
-                      f"{self.solution} at "
-                      f"step"
-                      f" {self.solution_step}")
-        if req_stop[0] == 0:
-            self._req_pause = True
+        while True:
+            raw_cost = self.cost_in.recv()
+            self.acknowledgement.send(np.asarray([1]))
+            req_stop = self.req_stop_in.recv()
+            self.acknowledgement.send(np.asarray([1]))
+            cost = [0]
+            if raw_cost[0]:
+                # The following casts cost as a signed 24-bit value (8 = 32 - 24)
+                cost = (raw_cost.astype(np.int32) << 8) >> 8
+            if cost[0]:
+                raw_solution = self.read_solution.recv()
+                # ToDo: The following way of reading out solutions only works
+                #  when the solutions are binary, i.e., QUBO problems. It relies
+                #  on the assumption that `solution' is the spiking history of the
+                #  neurons solving a problem and picks the spiking history from 3
+                #  timesteps ago, when the minimum cost was actually achieved.
+                self.solution[:] = (raw_solution.astype(np.int8) >> 2) & 1
+                self.min_cost = cost[0]
+                if req_stop[0] != 0:
+                    self.solution_step = req_stop[0]
+                    print(f"Host: received a better solution: "
+                        f"{self.solution} at "
+                        f"step"
+                        f" {self.solution_step}")
+            #if req_stop[0] == 0:
+            #    self._req_pause = True
 
-        # Post guard
-        if self.min_cost is not None:
-            if self.min_cost <= self.target_cost:
-                print("Host: LMT notified network reached target cost:",
-                      self.min_cost)
-                # Post mgmt
-                print("Host: stopping simulation at step:", self.solution_step)
-                self._req_pause = True
+            if self.min_cost is not None:
+                if self.min_cost <= self.target_cost:
+                    print("Host: LMT notified network reached target cost:",
+                        self.min_cost)
+                    # Post mgmt
+                    print("Host: stopping simulation at step:", self.solution_step)
+                    # self._req_pause = True
+                    return

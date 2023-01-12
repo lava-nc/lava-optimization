c8434a19e5f8acd08b3425b2ec248a08ea1e539f
Xu, He <he.xu@intel.com>
2023-01-12 07:54:32 +0000

    Ignore 1 line lint error
    Signed-off-by: Xu, He <he.xu@intel.com>

--- tests/lava/tutorials/test_tutorials.py
+++ tests/lava/tutorials/test_tutorials.py
@@ -101,7 +101,7 @@ class TestTutorials(unittest.TestCase):
             subprocess.check_call(args, env=env)  # noqa: S603
 
             fout.seek(0)
-            return subprocess.run(["ipython", "-c", fout.read()], env=env)
+            return subprocess.run(["ipython", "-c", fout.read()], env=env)  # noqa
 
     def _collect_errors_from_all_cells(self, result) \
             -> ty.List[str]:

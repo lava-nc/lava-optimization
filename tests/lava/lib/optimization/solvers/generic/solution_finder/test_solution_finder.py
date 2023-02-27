220788e5b4eff4f1f2aaa51539cfcdf638312197
GaboFGuerra <gabriel.fonseca.guerra@intel.com>
2023-02-27 16:33:38 +0100

    Set default value for num_in_ports.
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>

--- tests/lava/lib/optimization/solvers/generic/solution_finder/test_solution_finder.py
+++ tests/lava/lib/optimization/solvers/generic/solution_finder/test_solution_finder.py
@@ -5,9 +5,8 @@ import unittest
 
 import numpy as np
 from lava.lib.optimization.problems.problems import QUBO
-from lava.lib.optimization.solvers.generic.read_gate.models import (
-    ReadGatePyModel,
-)
+from lava.lib.optimization.solvers.generic.read_gate.models import \
+    get_read_gate_model_class
 from lava.lib.optimization.solvers.generic.read_gate.process import ReadGate
 from lava.lib.optimization.solvers.generic.solution_finder.process import (
     SolutionFinder,
@@ -48,6 +47,7 @@ class TestSolutionFinder(unittest.TestCase):
         )
 
         # Execution configurations.
+        ReadGatePyModel = get_read_gate_model_class(1)
         pdict = {ReadGate: ReadGatePyModel}
         self.run_cfg = Loihi2SimCfg(exception_proc_model_map=pdict)
         self.solution_finder._log_config.level = 20

4bc3ae6cce6a925274e5e616fc50f13c908b8f21
GaboFGuerra <gabriel.fonseca.guerra@intel.com>
2023-03-09 23:08:06 +0100

    Add explicit class to test compiler behaviour on CICD.
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>

--- src/lava/lib/optimization/solvers/generic/read_gate/models.py
+++ src/lava/lib/optimization/solvers/generic/read_gate/models.py
@@ -102,3 +102,58 @@ def get_read_gate_model_class(num_in_ports: int):
 
 
 ReadGatePyModel = get_read_gate_model_class(num_in_ports=1)
+
+
+
+@implements(ReadGate, protocol=LoihiProtocol)
+@requires(CPU)
+class ReadGatePyModelD(PyLoihiProcessModel):
+    """CPU model for the ReadGate process.
+
+    The model verifies if better payload (cost) has been notified by the
+    downstream processes, if so, it reads those processes state and sends out to
+    the upstream process the new payload (cost) and the network state.
+    """
+    target_cost: int = LavaPyType(int, np.int32, 32)
+    best_solution: int = LavaPyType(int, np.int32, 32)
+    cost_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.int32,
+                                   precision=32)
+    cost_out: PyOutPort = LavaPyType(
+        PyOutPort.VEC_DENSE, np.int32, precision=32
+    )
+    solution_out: PyOutPort = LavaPyType(
+        PyOutPort.VEC_DENSE, np.int32, precision=32
+    )
+    send_pause_request: PyOutPort = LavaPyType(
+        PyOutPort.VEC_DENSE, np.int32, precision=32
+    )
+    solution_reader = LavaPyType(PyRefPort.VEC_DENSE, np.int32, precision=32)
+    min_cost: int = None
+    solution: np.ndarray = None
+
+    def post_guard(self):
+        """Decide whether to run post management phase."""
+        return True if self.min_cost else False
+
+    def run_spk(self):
+        """Execute spiking phase, integrate input, update dynamics and
+        send messages out."""
+        cost = self.cost_in.recv()
+        if cost[0]:
+            self.min_cost = cost[0]
+            self.cost_out.send(np.array([0]))
+        elif self.solution is not None:
+            timestep = - np.array([self.time_step - 2])
+            if self.min_cost <= self.target_cost:
+                self._req_pause = True
+            self.cost_out.send(np.array([self.min_cost]))
+            self.send_pause_request.send(timestep)
+            self.solution_out.send(self.solution)
+            self.solution = None
+            self.min_cost = None
+        else:
+            self.cost_out.send(np.array([0]))
+
+    def run_post_mgmt(self):
+        """Execute post management phase."""
+        self.solution = self.solution_reader.read()

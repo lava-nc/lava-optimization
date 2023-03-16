612abfe8ecb11edc4ccbdfb6f1c9f95f931e2ad8
Gavin Parpart <gavin.parpart@pnnl.gov>
2023-03-16 13:11:52 -0700

    Add 1-Layer and 2-Layer LCA Implementation

---
+++ src/lava/lib/optimization/solvers/lca/lca_neuron/models.py
@@ -0,0 +1,55 @@
+#  Copyright (C) 2023 Battelle Memorial Institute
+#  SPDX-License-Identifier: BSD-2-Clause
+#  See: https://spdx.org/licenses/
+
+import numpy as np
+from lava.magma.core.decorator import implements, requires, tag
+from lava.magma.core.model.py.model import PyLoihiProcessModel
+from lava.magma.core.model.py.ports import PyInPort, PyOutPort
+from lava.magma.core.model.py.type import LavaPyType
+from lava.magma.core.resources import CPU
+from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
+
+from lava.lib.optimization.solvers.lca.lca_neuron.process import LCANeuron
+
+
+@implements(proc=LCANeuron, protocol=LoihiProtocol)
+@requires(CPU)
+@tag('floating_pt')
+class PyLCANeuronFloat(PyLoihiProcessModel):
+    # This model might spike too frequently. Implement an accumulator if so.
+    vth: float = LavaPyType(float, float)
+    tau: float = LavaPyType(float, float)
+    a_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, float)
+    s_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, float)
+    v: np.ndarray = LavaPyType(np.ndarray, float)
+    bias: np.ndarray = LavaPyType(np.ndarray, float)
+
+    def run_spk(self):
+        # Soft-threshold activation
+        activation = np.maximum(np.abs(self.v) - self.vth, 0) * np.sign(self.v)
+        bias = activation * self.tau if self.proc_params['two_layer'] else \
+            self.bias
+        self.v = self.v * (1 - self.tau) + self.a_in.recv() + bias
+        self.s_out.send(activation)
+
+
+@implements(proc=LCANeuron, protocol=LoihiProtocol)
+@requires(CPU)
+@tag('fixed_pt')
+class PyLCANeuronFixed(PyLoihiProcessModel):
+    vth: int = LavaPyType(int, int)
+    tau: int = LavaPyType(int, int)
+    a_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, int)
+    s_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, int)
+    v: np.ndarray = LavaPyType(np.ndarray, int)
+    bias: np.ndarray = LavaPyType(np.ndarray, int)
+
+    def run_spk(self):
+        # Soft-threshold activation
+        activation = np.maximum(np.abs(self.v) - self.vth, 0) * np.sign(self.v)
+        bias = np.right_shift(activation * self.tau, 24) \
+            if self.proc_params['two_layer'] else self.bias
+        self.v = np.right_shift(self.v * (2 ** 24 - self.tau), 24) \
+            + self.a_in.recv() + bias
+        self.s_out.send(activation)
---
+++ src/lava/lib/optimization/solvers/lca/models.py
@@ -0,0 +1,154 @@
+#  Copyright (C) 2023 Battelle Memorial Institute
+#  SPDX-License-Identifier: BSD-2-Clause
+#  See: https://spdx.org/licenses/
+
+import numpy as np
+
+from lava.magma.core.model.sub.model import AbstractSubProcessModel
+
+from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
+from lava.magma.core.decorator import implements, tag
+from lava.proc.dense.process import Dense
+
+from lava.lib.optimization.solvers.lca.process import LCA1Layer, LCA2Layer
+from lava.lib.optimization.solvers.lca.lca_neuron.process import LCANeuron
+from lava.lib.optimization.solvers.lca.accumulator.process import AccumulatorNeuron
+
+
+@implements(proc=LCA2Layer, protocol=LoihiProtocol)
+@tag('floating_pt')
+class LCA2LayerModelFloat(AbstractSubProcessModel):
+    def __init__(self, proc: LCA2Layer):
+        threshold = proc.threshold.get()
+        T = proc.tau.get()
+        T_exp = proc.tau_exp.get()
+        T_float = np.ldexp(T, T_exp)
+
+        weights = proc.weights.get()
+        weights_exp = proc.weights_exp.get()
+        input_val = proc.input.get()
+        spike_height = proc.spike_height.get()
+
+        self.v1 = LCANeuron(shape=(weights.shape[0],), tau=T_float,
+                            vth=threshold, two_layer=True)
+        # weight_exp shifted 8 bits for the weights, 6 for the v1 output.
+        self.weights_T = Dense(weights=-weights.T, num_message_bits=24,
+                               weight_exp=weights_exp)
+
+        self.res = AccumulatorNeuron(shape=(weights.shape[1],),
+                                     spike_height=spike_height, bias=input_val)
+
+        self.weights = Dense(weights=(weights * T), num_message_bits=24,
+                             weight_exp=weights_exp + T_exp)
+
+        self.weights.a_out.connect(self.v1.a_in)
+        self.res.s_out.connect(self.weights.s_in)
+
+        self.weights_T.a_out.connect(self.res.a_in)
+        self.v1.s_out.connect(self.weights_T.s_in)
+
+        # Expose output and voltage
+        self.v1.s_out.connect(proc.out_ports.v1)
+        self.res.s_out.connect(proc.out_ports.res)
+        proc.vars.voltage.alias(self.v1.vars.v)
+        proc.vars.input.alias(self.res.bias)
+
+
+@implements(proc=LCA1Layer, protocol=LoihiProtocol)
+@tag('floating_pt')
+class LCA1LayerModelFloat(AbstractSubProcessModel):
+    def __init__(self, proc: LCA1Layer):
+        threshold = proc.threshold.get()
+        T = proc.tau.get()
+        T_exp = proc.tau_exp.get()
+        T_float = np.ldexp(T, T_exp)
+
+        weights = proc.weights.get()
+        weights_exp = proc.weights_exp.get()
+        bias = proc.bias.get()
+
+        self.v1 = LCANeuron(shape=(weights.shape[0],), tau=T_float,
+                            vth=threshold, bias=bias, two_layer=False)
+        # weight_exp shifted 8 bits for the weights, 6 for the v1 output.
+        self.weights = Dense(weights=weights, num_message_bits=24,
+                             weight_exp=weights_exp)
+
+        self.weights.a_out.connect(self.v1.a_in)
+        self.v1.s_out.connect(self.weights.s_in)
+
+        # Expose output and voltage
+        self.v1.s_out.connect(proc.out_ports.v1)
+        proc.vars.voltage.alias(self.v1.vars.v)
+        proc.vars.bias.alias(self.v1.bias)
+
+
+@implements(proc=LCA2Layer, protocol=LoihiProtocol)
+@tag('fixed_pt')
+class LCA2LayerModelFixed(AbstractSubProcessModel):
+    def __init__(self, proc: LCA2Layer):
+        threshold = proc.threshold.get()
+        T = proc.tau.get()
+        T_exp = proc.tau_exp.get()
+        T_int = int(np.ldexp(T, T_exp + 24))
+
+        weights = proc.weights.get()
+        weights_exp = proc.weights_exp.get()
+        input_val = proc.input.get()
+        spike_height = proc.spike_height.get()
+
+        assert np.issubdtype(input_val.dtype, np.integer),\
+            "Input must be an integer"
+
+        self.v1 = LCANeuron(shape=(weights.shape[0],), tau=T_int, vth=threshold,
+                            two_layer=True)
+
+        self.weights_T = Dense(weights=-weights.T, num_message_bits=24,
+                               weight_exp=weights_exp)
+
+        self.res = AccumulatorNeuron(shape=(weights.shape[1],),
+                                     spike_height=spike_height, bias=input_val)
+
+        self.weights = Dense(weights=(weights * T), num_message_bits=24,
+                             weight_exp=weights_exp + T_exp)
+
+        self.weights.a_out.connect(self.v1.a_in)
+        self.res.s_out.connect(self.weights.s_in)
+
+        self.weights_T.a_out.connect(self.res.a_in)
+        self.v1.s_out.connect(self.weights_T.s_in)
+
+        # Expose output and voltage
+        self.v1.s_out.connect(proc.out_ports.v1)
+        self.res.s_out.connect(proc.out_ports.res)
+        proc.vars.voltage.alias(self.v1.vars.v)
+        proc.vars.input.alias(self.res.bias)
+
+
+@implements(proc=LCA1Layer, protocol=LoihiProtocol)
+@tag('fixed_pt')
+class LCA1LayerModelFixed(AbstractSubProcessModel):
+    def __init__(self, proc: LCA1Layer):
+        threshold = proc.threshold.get()
+        T = proc.tau.get()
+        T_exp = proc.tau_exp.get()
+        T_int = int(np.ldexp(T, T_exp + 24))
+
+        weights = proc.weights.get()
+        weights_exp = proc.weights_exp.get()
+        bias = proc.bias.get()
+
+        assert np.issubdtype(bias.dtype, np.integer), "Bias must be an integer"
+
+        self.v1 = LCANeuron(shape=(weights.shape[0],), tau=T_int, vth=threshold,
+                            bias=bias, two_layer=False)
+        # weight_exp shifted 8 bits for the weights, 6 for the v1 output.
+        self.weights = Dense(weights=weights, num_message_bits=24,
+                             weight_exp=weights_exp)
+
+        self.weights.a_out.connect(self.v1.a_in)
+        self.v1.s_out.connect(self.weights.s_in)
+
+        # Expose output and voltage
+        self.v1.s_out.connect(proc.out_ports.v1)
+        proc.vars.voltage.alias(self.v1.vars.v)
+        proc.vars.bias.alias(self.v1.bias)

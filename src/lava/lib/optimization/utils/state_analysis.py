bb70eca6c29642f185603fbd4c41707fcc9218cb
Pierro, Alessandro <alessandro.pierro@intel.com>
2023-05-08 13:58:38 +0200

    Update plots to use seaborn

--- src/lava/lib/optimization/utils/state_analysis.py
+++ src/lava/lib/optimization/utils/state_analysis.py
@@ -3,12 +3,18 @@
 # See: https://spdx.org/licenses/
 
 
+import typing as ty
+import warnings
+
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+from matplotlib.figure import Figure
+
 from lava.lib.optimization.problems.problems import QUBO
 from lava.lib.optimization.solvers.generic.solver import SolverReport
 
-from matplotlib import pyplot as plt
-import warnings
-import numpy as np
+sns.set_style("whitegrid")
 
 
 class StateAnalysis:
@@ -19,22 +25,37 @@ class StateAnalysis:
         self,
         x: np.ndarray = None,
         y: np.ndarray = None,
-        title: str = None,
-    ) -> plt.Axes:
-        ax = plt.figure(figsize=(10, 10)).add_subplot(1, 1, 1)
+        xlabel: str = "",
+        ylabel: str = "",
+        xlim: ty.Tuple[float, float] = None,
+        ylim: ty.Tuple[float, float] = None,
+        ax: plt.Axes = None,
+    ) -> Figure:
+        if ax is None:
+            ax = plt.figure(figsize=(4, 2)).add_subplot(1, 1, 1)
         ax.plot(x, y)
-        ax.set_title(title)
-        return ax
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_xlim(xlim)
+        ax.set_ylim(ylim)
+        return ax.get_figure()
 
     def _hist_plot(
         self,
         y: np.ndarray = None,
-        title: str = None,
-    ) -> plt.Axes:
-        ax = plt.figure(figsize=(10, 10)).add_subplot(1, 1, 1)
-        ax.hist(y)
-        ax.set_title(title)
-        return ax
+        xlabel: str = "",
+        ylabel: str = "% of Samples",
+        ylim: ty.Tuple[float, float] = None,
+        bins: int = 12,
+        ax: plt.Axes = None,
+    ) -> Figure:
+        if ax is None:
+            ax = plt.figure(figsize=(4, 2)).add_subplot(1, 1, 1)
+        sns.histplot(y, bins=bins, stat="percent", ax=ax)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_ylim(ylim)
+        return ax.get_figure()
 
     def _extract_costs_or_warn(self, report: SolverReport) -> np.ndarray:
         if report.cost_timeseries is not None:
@@ -52,11 +73,11 @@ class StateAnalysis:
         else:
             warnings.warn("State timeseries is not available.")
 
-    def _show_or_save(self, ax: plt.Axes, filename: str = None) -> None:
+    def _show_or_save(self, fig: Figure, filename: str = None) -> None:
         if filename is None:
-            ax.get_figure().show()
+            fig.show()
         else:
-            ax.get_figure().savefig(filename)
+            fig.savefig(filename, bbox_inches="tight", dpi=600)
 
     def plot_cost_timeseries(
         self, report: SolverReport, filename: str = None
@@ -64,8 +85,10 @@ class StateAnalysis:
         cost = self._extract_costs_or_warn(report)
         if cost is None:
             return
-        ax = self._line_plot(x=list(range(len(cost))), y=cost, title="Cost")
-        self._show_or_save(ax, filename)
+        fig = self._line_plot(
+            x=list(range(len(cost))), y=cost, xlabel="Timestep", ylabel="Cost"
+        )
+        self._show_or_save(fig, filename)
 
     def plot_min_cost_timeseries(
         self, report: SolverReport, filename: str = None
@@ -74,10 +97,13 @@ class StateAnalysis:
         if cost is None:
             return
         min_cost = np.minimum.accumulate(cost, axis=0)
-        ax = self._line_plot(
-            x=list(range(len(min_cost))), y=min_cost, title="Minimum Cost"
+        fig = self._line_plot(
+            x=list(range(len(min_cost))),
+            y=min_cost,
+            xlabel="Timestep",
+            ylabel="Cumulative Best Cost",
         )
-        self._show_or_save(ax, filename)
+        self._show_or_save(fig, filename)
 
     def plot_cost_distribution(
         self, report: SolverReport, filename: str = None
@@ -85,8 +111,8 @@ class StateAnalysis:
         cost = self._extract_costs_or_warn(report)
         if cost is None:
             return
-        ax = self._hist_plot(y=cost, title="Cost Distribution")
-        self._show_or_save(ax, filename)
+        fig = self._hist_plot(y=cost, xlabel="Cost", ylim=(-4, 104))
+        self._show_or_save(fig, filename)
 
     def plot_delta_cost_distribution(
         self, report: SolverReport, filename: str = None
@@ -95,8 +121,10 @@ class StateAnalysis:
         if cost is None:
             return
         delta_cost = cost[1:] - cost[:-1]
-        ax = self._hist_plot(y=delta_cost, title="Delta-Cost Distribution")
-        self._show_or_save(ax, filename)
+        fig = self._hist_plot(
+            y=delta_cost, xlabel="Energy Transition", ylim=(-4, 104)
+        )
+        self._show_or_save(fig, filename)
 
     def plot_unique_state_visits(
         self, report: SolverReport, filename: str = None
@@ -106,16 +134,19 @@ class StateAnalysis:
             return
         num_unique_states = list(
             map(
-                lambda i: np.unique(states[:i], axis=0).shape[0],
+                lambda i: np.unique(states[:i], axis=0).shape[0]
+                / (2**self.problem.num_variables)
+                * 100,
                 range(len(states)),
             )
         )
-        ax = self._line_plot(
+        fig = self._line_plot(
             x=list(range(len(states))),
             y=num_unique_states,
-            title="Cumulative Number of Unique States Visited",
+            xlabel="Timestep",
+            ylabel="% of State Space Visited",
         )
-        self._show_or_save(ax, filename)
+        self._show_or_save(fig, filename)
 
     def plot_successive_states_distance(
         self, report: SolverReport, filename: str = None
@@ -123,10 +154,17 @@ class StateAnalysis:
         states = self._extract_states_or_warn(report)
         if states is None:
             return
-        distance = np.sum(np.abs(states[1:] - states[:-1]), axis=1)
-        ax = self._line_plot(
+        distance = (
+            np.sum(np.abs(states[1:] - states[:-1]), axis=1)
+            / self.problem.num_variables
+            * 100
+        )
+        fig = self._line_plot(
             x=list(range(len(states) - 1)),
             y=distance,
-            title="Hamming Distance Between Successive States",
+            ylabel="% of Bit Flips",
+            xlabel="Timestep",
         )
-        self._show_or_save(ax, filename)
+        self._show_or_save(fig, filename)
+
+    def plot_variables_moving_average(

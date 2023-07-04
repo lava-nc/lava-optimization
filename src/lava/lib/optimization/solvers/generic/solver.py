Fix integer overflow bug (#235)
* Deleting incorrect reference to NcProcModels

* Adding revised SA model

* Revised cost_integrator, running but not validated

* Read_Gate working with two input ports / 32bit precision

* Integrated 32bit cost into QUBO solver

* Integrated 32bit into CPU QUBO solver

* add _byte(s) to var/port names

---------

Co-authored-by: Alessandro Pierro <alessandro.pierro@intel.com>

src/lava/lib/optimization/solvers/generic/solver.py
@@ -220,12 +220,15 @@ class OptimizationSolver:
         self.solver_process = None
         self.solver_model = None
         self._profiler = None
-        self._cost_tracker = None
+        self._cost_tracker_first_byte = None
+        self._cost_tracker_last_bytes = None
         self._state_tracker = None
 
     def solve(self, config: SolverConfig = SolverConfig()) -> SolverReport:
         """
-        Create solver from problem spec and run until target_cost or timeout.
+        Create solver from problem spec and run until it has
+        either minimized the cost to the target_cost or ran for a number of
+        time steps provided by the timeout parameter.
 
         Parameters
         ----------
@@ -262,8 +265,12 @@ class OptimizationSolver:
             from lava.utils.loihi2_state_probes import StateProbe
 
             if config.probe_cost:
-                self._cost_tracker = StateProbe(self.solver_process.optimality)
-                probes.append(self._cost_tracker)
+                self._cost_tracker_last_bytes = StateProbe(
+                    self.solver_process.optimality_last_bytes)
+                self._cost_tracker_first_byte = StateProbe(
+                    self.solver_process.optimality_first_byte)
+                probes.append(self._cost_tracker_last_bytes)
+                probes.append(self._cost_tracker_first_byte)
             if config.probe_state:
                 self._state_tracker = StateProbe(
                     self.solver_process.variable_assignment
@@ -271,12 +278,18 @@ class OptimizationSolver:
                 probes.append(self._state_tracker)
         elif config.backend in CPUS:
             if config.probe_cost:
-                self._cost_tracker = Monitor()
-                self._cost_tracker.probe(
-                    target=self.solver_process.optimality,
+                self._cost_tracker_first_byte = Monitor()
+                self._cost_tracker_first_byte.probe(
+                    target=self.solver_process.optimality_first_byte,
                     num_steps=config.timeout,
                 )
-                probes.append(self._cost_tracker)
+                self._cost_tracker_last_bytes = Monitor()
+                self._cost_tracker_last_bytes.probe(
+                    target=self.solver_process.optimality_last_bytes,
+                    num_steps=config.timeout,
+                )
+                probes.append(self._cost_tracker_first_byte)
+                probes.append(self._cost_tracker_last_bytes)
             if config.probe_state:
                 self._state_tracker = Monitor()
                 self._state_tracker.probe(
@@ -343,9 +356,19 @@ class OptimizationSolver:
         config: SolverConfig
             Solver configuraiton used. Refers to SolverConfig documentation.
         """
-        cost_timeseries = self._get_probed_data(
-            tracker=self._cost_tracker, var_name="optimality"
+        cost_timeseries_last_bytes = self._get_probed_data(
+            tracker=self._cost_tracker_last_bytes,
+            var_name="optimality_last_bytes"
         )
+        cost_timeseries_first_byte = self._get_probed_data(
+            tracker=self._cost_tracker_first_byte,
+            var_name="optimality_first_byte"
+        )
+        if self._cost_tracker_first_byte is not None:
+            cost_timeseries = (cost_timeseries_first_byte << 24) + \
+                cost_timeseries_last_bytes
+        else:
+            cost_timeseries = None
         state_timeseries = self._get_probed_data(
             tracker=self._state_tracker, var_name="variable_assignment"
         )
@@ -435,4 +458,4 @@ class OptimizationSolver:
             return raw_solution.astype(np.int8) >> 5
         else:
             best_assignment = self.solver_process.best_variable_assignment
-            return best_assignment.aliased_var.get()
+            return best_assignment.aliased_var.get().astype(np.int8)
c4dce2173a27a2b6fdb439de04d0677660b872ab
Risbud, Sumedh <sumedh.risbud@intel.com>
2023-09-22 16:53:10 -0700

    Minor changes to a scratch test
    Signed-off-by: Risbud, Sumedh <sumedh.risbud@intel.com>

--- src/lava/lib/optimization/apps/scheduler/test.py
+++ src/lava/lib/optimization/apps/scheduler/test.py
@@ -1,5 +1,6 @@
 import pprint
 
+import numpy as np
 from matplotlib import pyplot as plt
 
 from lava.lib.optimization.apps.scheduler.problems import (
@@ -10,26 +11,28 @@ from lava.lib.optimization.apps.scheduler.solver import (Scheduler,
 
 if __name__ == "__main__":
     if True:
-        ssp = SatelliteScheduleProblem(num_satellites=12,
-                                       num_requests=100)
+        ssp = SatelliteScheduleProblem(num_satellites=3,
+                                       num_requests=100,
+                                       view_height=0.75)
         ssp.generate(42)
         ssp.plot_problem()
 
         sat_scheduler = SatelliteScheduler(ssp,
-                                           qubo_weights=(4, 20),
+                                           qubo_weights=(1, 8),
                                            probe_loihi_exec_time=True)
 
-        sat_scheduler.solve_with_netx()
-        print(f'Scheduled {sat_scheduler.netx_solution.shape[0]} Requests.')
+        # sat_scheduler.solve_with_netx()
+        # print(f'Scheduled {sat_scheduler.netx_solution.shape[0]} Requests.')
 
-        sat_scheduler.qubo_hyperparams = ({"neuron_model": "nebm-sa-refract",
-                                           "temperature": 1,
-                                           "refract": 64,
-                                           "refract_scaling": 1,
-                                           "max_temperature": 5,
-                                           "min_temperature": 1,
-                                           "steps_per_temperature": 200},
+        sat_scheduler.qubo_hyperparams = ({"temperature": 1},
                                           True)
+        # {"neuron_model": "nebm-sa-refract",
+        #  "temperature": 1,
+        #  "refract": 64,
+        #  "refract_scaling": 1,
+        #  "max_temperature": 5,
+        #  "min_temperature": 1,
+        #  "steps_per_temperature": 200}
         sat_scheduler.lava_backend = "Loihi2"
         sat_scheduler.solve_with_lava_qubo(timeout=1000)
         print(f'Scheduled {sat_scheduler.lava_solution.shape[0]} Requests.')

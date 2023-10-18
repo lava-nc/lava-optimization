929e036fcd63a57508ef06f79c93eaaf329bc5d8
Stratmann, Philipp <philipp.stratmann@intel.com>
2023-10-18 17:59:31 +0200

    Proc and ProcModel for new neuron model

--- src/lava/lib/optimization/solvers/generic/nebm/process.py
+++ src/lava/lib/optimization/solvers/generic/nebm/process.py
@@ -61,10 +61,18 @@ class NEBM(AbstractProcess):
             return self.proc_params["shape"]
 
 
-class NEBMSimulatedAnnealing(AbstractProcess):
+class NEBMSimulatedAnnealingLocal(AbstractProcess):
     """
     Non-equilibrium Boltzmann (NEBM) neuron model to solve QUBO problems.
+    This model uses purely information available at the level of individual
+    neurons to decide whether to switch or not, in contrast to the inheriting
+    Process NEBMSimulatedAnnealing.
     """
+
+    enabled_neuron_models = ['nebm-sa-refract']
+    deprecated_neuron_models = ['nebm-sa-refract-approx',
+                                'nebm-sa-balanced',
+                                'nebm-sa-refract-approx-unbalanced']
 
     def __init__(
         self,
@@ -72,7 +80,7 @@ class NEBMSimulatedAnnealing(AbstractProcess):
         shape: ty.Tuple[int, ...],
         cost_diagonal: npty.ArrayLike,
         max_temperature: int,
-        refract_scaling: int,
+        refract_scaling: ty.Optional[int],
         init_value=0,
         init_state=None,
         neuron_model: str,
@@ -96,6 +104,9 @@ class NEBMSimulatedAnnealing(AbstractProcess):
             The spiking history with which the network is initialized
         init_state : ArrayLike
             The state of neurons with which the network is initialized
+        neuron_model : str
+            The neuron model to be used. The latest list of allowed values
+            can be found in NEBMSimulatedAnnealing.enabled_neuron_models.
         """
 
         self._validate_input(neuron_model)
@@ -103,6 +114,7 @@ class NEBMSimulatedAnnealing(AbstractProcess):
             shape=shape,
             cost_diagonal=cost_diagonal,
             refract_scaling=refract_scaling,
+            neuron_model=neuron_model,
         )
 
         self.a_in = InPort(shape=shape)
@@ -135,34 +147,77 @@ class NEBMSimulatedAnnealing(AbstractProcess):
         """Validates input. At the moment, it only checks that the user has
         chosen a supported neuron model is """
 
-        if neuron_model == 'nebm-sa-refract':
+        if neuron_model in self.enabled_neuron_models:
             return
-        elif neuron_model == 'nebm-sa':
+        elif neuron_model in self.deprecated_neuron_models:
             raise NotImplementedError(
-                f"The model 'nebm-sa' has been deprecated. Instead, we "
+                f"The model {neuron_model} has been deprecated. Instead, we "
                 f"recommend switching to the new neuron model 'nebm-sa-refract'"
-                f" for a better solver performance.")
-        elif neuron_model == 'nebm-sa-balanced':
-            raise NotImplementedError(
-                f"The model 'nebm-sa-balanced' has been deprecated. Instead, we"
-                f" recommend switching to the new neuron model "
-                f"'nebm-sa-refract' for a better solver performance.")
-        elif neuron_model == 'nebm-sa-refract-approx-unbalanced':
-            raise NotImplementedError(
-                f"Please note that the neuron model "
-                f"'nebm-sa-refract_approx_unbalanced' has been removed. "
-                f"Instead, we recommend switching to the new neuron model "
-                f"'nebm-sa-refract' for a better solver performance.")
-        elif neuron_model == 'nebm-sa-refract-approx':
-            raise NotImplementedError(
-                f"Please note that the neuron model "
-                f"'nebm-sa-refract_approx_unbalanced' has been removed Instead,"
-                f" we recommend switching to the neuron model 'nebm-sa-refract'"
                 f" for a better solver performance.")
         else:
             raise ValueError(
-                f"Please specify a correct neuron model as hyperparameter.")
+                f"The model {neuron_model} does not exist. Please specify a "
+                f"correct neuron model as hyperparameter.")
 
     @property
     def shape(self) -> ty.Tuple[int, ...]:
         return self.proc_params["shape"]
+
+
+class NEBMSimulatedAnnealing(NEBMSimulatedAnnealingLocal):
+    """
+    Non-equilibrium Boltzmann (NEBM) neuron model to solve QUBO problems.
+    This model combines the switching intentions of all NEBM neurons to
+    decide whether to switch or not, to avoid conflicting variable switches.
+    """
+
+    enabled_neuron_models = ['nebm-sa', 'nebm-sa-refract']
+
+    def __init__(
+        self,
+        *,
+        shape: ty.Tuple[int, ...],
+        cost_diagonal: npty.ArrayLike,
+        max_temperature: int,
+        init_value=0,
+        init_state=None,
+        neuron_model: str,
+    ):
+        """
+        SA Process.
+
+        Parameters
+        ----------
+        shape: Tuple
+            Number of neurons. Default is (1,).
+
+        refract_scaling : ArrayLike
+            After a neuron has switched its binary variable, it remains in a
+            refractory state that prevents any variable switching for a
+            number of time steps. This number of time steps is determined by
+                rand(0, 255) >> refract_scaling
+            Refract_scaling thus denotes the order of magnitude of timesteps a
+            neuron remains in a state after a transition.
+        init_value : ArrayLike
+            The spiking history with which the network is initialized
+        init_state : ArrayLike
+            The state of neurons with which the network is initialized
+        neuron_model : str
+            The neuron model to be used. The latest list of allowed values
+            can be found in NEBMSimulatedAnnealing.enabled_neuron_models.
+        """
+
+        super().__init__(
+            shape=shape,
+            cost_diagonal=cost_diagonal,
+            refract_scaling=None,
+            max_temperature=max_temperature,
+            init_value=init_value,
+            init_state=init_state,
+            neuron_model=neuron_model,
+        )
+
+        # number of NEBM neurons that suggest switching in a time step
+        self.n_switches_in = InPort(shape=shape)
+        # port to notify other NEBM neurons of switching intentions
+        self.suggest_switch_out = OutPort(shape=shape)
